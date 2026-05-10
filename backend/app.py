f8e2183ac141d14d98489a116f63003e2bf15c4d
Vu Luong <luongngocvu168@gmail.com>
2026-05-10 15:52:28 +0700

    feat: enhance backend with database configuration and user model

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,42 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-@app.route("/")
-def home():
-    return {"message": "hello aws 123"}
+DB_URL = (
+    f"postgresql+pg8000://{DB_USER}:{DB_PASSWORD}" f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, db.Identity(start=1, increment=1), primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True, index=True)
+    created_at = db.Column(
+        db.DateTime, nullable=True, server_default=db.func.current_timestamp()
+    )
+
+
+@app.route("/users")
+def get_users():
+    users = User.query.all()
+
+    return jsonify([{"id": u.id, "name": u.name, "email": u.email} for u in users])
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
